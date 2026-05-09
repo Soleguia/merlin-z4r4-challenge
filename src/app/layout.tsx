8fd2bd0ea07c72d49dab2ffb133d0b5fd648d4ac
Josu <soleguia@gmail.com>
2026-05-09 22:23:47 +0200

    fix: html lang='en'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es">
+    <html lang="en">
       <body>
         <a href="#main-content" className="skip-link">
           Skip to main content
